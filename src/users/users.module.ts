4fd2617d4ee980bc94ae1fdf6582cc3b8c931fa6
Jimyung Koh <kojimyung1206@naver.com>
2023-02-15 21:37:13 +0900

    refactor: 불필요한 메서드 제거

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
-<<<<<<< Updated upstream
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-=======
->>>>>>> Stashed changes
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-<<<<<<< Updated upstream
-  controllers: [UsersController],
-  providers: [UsersService],
-=======
->>>>>>> Stashed changes
 })
 export class UsersModule {}
