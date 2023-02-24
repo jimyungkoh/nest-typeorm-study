99170dd3f321a586a07afc94334c8d82d77a68d3
Jimyung Koh <kojimyung1206@naver.com>
2023-02-24 16:20:51 +0900

    refactor: 불필요한 클래스 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [UsersModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
