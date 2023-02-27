e65b9cc78d019705cd020c03f7634e9220f70782
Jimyung Koh <kojimyung1206@naver.com>
2023-02-27 21:00:07 +0900

    feat: 서비스 예외 생성
    - 커스텀 예외 필터의 타겟
    - ServiceExeption 타입 인스턴스 생성 메서드 생성

---
+++ src/common/exception/service.exception.ts
@@ -0,0 +1,27 @@
+import { ENTITY_NOT_FOUND, ErrorCode, PERMISSION_DENIED } from './error-code';
+
+// ENTITY_NOT_FOUND 값 객체(status, default-message)를 가진
+//  ServiceException 인스턴스 생성 메서드
+export const EntityNotFoundException = (message?: string) => {
+  return new ServiceException(ENTITY_NOT_FOUND, message);
+};
+
+// PERMISSION_DENIED 값 객체를 가진
+//  ServiceException 인스턴스 생성 메서드
+export const PermissionDeniedException = (message?: string) => {
+  return new ServiceException(PERMISSION_DENIED, message);
+};
+
+export class ServiceException extends Error {
+  readonly errorCode: ErrorCode;
+
+  constructor(errorCode: ErrorCode, message?: string) {
+    if (!message) {
+      message = errorCode.message;
+    }
+
+    super(message);
+
+    this.errorCode = errorCode;
+  }
+}
