9449fc9bfcf8f2ca28587051e2ab6cc19fb3f137
Jimyung Koh <kojimyung1206@naver.com>
2023-02-28 01:02:44 +0900

    refactor: ServiceException 인스턴스 생성 메서드의 리턴 타입 명시

--- src/common/exception/service.exception.ts
+++ src/common/exception/service.exception.ts
@@ -2,13 +2,15 @@ import { ENTITY_NOT_FOUND, ErrorCode, PERMISSION_DENIED } from './error-code';
 
 // ENTITY_NOT_FOUND 값 객체(status, default-message)를 가진
 //  ServiceException 인스턴스 생성 메서드
-export const EntityNotFoundException = (message?: string) => {
+export const EntityNotFoundException = (message?: string): ServiceException => {
   return new ServiceException(ENTITY_NOT_FOUND, message);
 };
 
 // PERMISSION_DENIED 값 객체를 가진
 //  ServiceException 인스턴스 생성 메서드
-export const PermissionDeniedException = (message?: string) => {
+export const PermissionDeniedException = (
+  message?: string,
+): ServiceException => {
   return new ServiceException(PERMISSION_DENIED, message);
 };
 
