2a72e0d21cb40e069be4642f7f464a31293fa6dd
Jimyung Koh <kojimyung1206@naver.com>
2023-02-27 21:01:13 +0900

    feat: 커스텀 예외 필터 전역 사용 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './common/database';
+import { ServiceExceptionToHttpExceptionFilter } from './common/exception-filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [UsersModule, DatabaseModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: ServiceExceptionToHttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
